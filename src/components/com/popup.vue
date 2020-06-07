a5cde7727a9cc6178cbd669f4dfb9aec79407897
mengfanqiang <mengfanqiang@mengfanqiangdeMacBook-Air.local>
2020-06-07 18:04:57 +0800

    左边弹窗ui开发

--- src/components/com/popup.vue
+++ src/components/com/popup.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="container">
-        <p>夜间模式</p>
+        <div class="header"></div>
+
+        <div class="setting_container">
+            <div class="setting_box" v-for="(item,index) in settingList" :key="index">
+               <p> {{ item.tit }}</p>
+            </div>
+        </div>
+
+        <div class="footer_setting">
+            <div>暗黑模式</div>
+            <div>设置</div>
+            <div>退出登录</div>
+        </div>
     </div>
 </template>
 
@@ -11,10 +23,88 @@ export default {
             type:Boolean,
             default:false
         }
+    },
+    data() {
+        return{
+            settingList:[
+                {
+                    tit:'演出'
+                },
+                {
+                    tit:'商城'
+                },
+                {
+                    tit:'附近的人'
+                },
+                {
+                    tit:'口袋铃彩'
+                },
+                {
+                    tit:'我的订单'
+                },
+                {
+                    tit:'定时停止播放'
+                },
+                {
+                    tit:'扫一扫'
+                },
+                {
+                    tit:'音乐闹钟'
+                },
+                {
+                    tit:'音乐云盘'
+                },
+                {
+                    tit:'在线听歌免流量'
+                },
+                {
+                    tit:'游戏推荐'
+                },
+                {
+                    tit:'优惠卷'
+                },
+                {
+                    tit:'加入网易音乐人'
+                },
+                {
+                    tit:'我要直播'
+                }
+            ]
+        }
+    },
+    methods:{
+        
     }
 }
 </script>
 
-<style scope>
+<style scoped>
 
+.container{
+    font-size: .32rem;
+    height: 100%;
+    color:#999;
+}
+.footer_setting{
+    height:6%;
+    border-top:1px solid #EEE9E9;
+    width:100%;
+    display: flex;
+    position: fixed;
+    bottom: 0;
+    align-items: center;
+    justify-content: space-around;
+}
+.header{
+    background:forestgreen;
+    width:100%;
+    height:30%;
+}
+.setting_container{
+    height:64%;
+    overflow-x: auto;
+}
+.setting_box{
+    padding:.25rem .5rem;
+}
 </style>
